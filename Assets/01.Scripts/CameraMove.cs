6c6fc2dc57bb6914b82e2cc8a80edc931b8b9135
gyeommi <kkm417417@naver.com>
2026-07-13 10:28:17 +0900

    feat: Add Camera Move
    - 카메라가 플레이어 따라다니는 기능 구현하기

---
+++ Assets/01.Scripts/CameraMove.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    Vector3 velocity;
+    Vector3 targetPos;
+
+    void Start()
+    {
+        velocity = Vector3.zero;
+    }
+
+    private void LateUpdate()
+    {
+        targetPos = new Vector3(target.position.x, target.position.y, transform.position.z);
+        float x = Mathf.Clamp(targetPos.x, -2f, 142f);
+        float y = Mathf.Clamp(targetPos.y, 0f, 7.5f);
+
+        targetPos = new Vector3(x, y, transform.position.z);
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, 0.2f);
+    }
+}
