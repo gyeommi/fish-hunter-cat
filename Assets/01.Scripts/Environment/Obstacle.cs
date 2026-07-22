0f353c4da3974b51a2e91a4b61b0e27e37ec64c8
gyeommi <kkm417417@naver.com>
2026-07-22 13:48:42 +0900

    feat: Update Stage3
    - 스테이지3 적, 바닥, 등등 완성하기

--- Assets/01.Scripts/Environment/Obstacle.cs
+++ Assets/01.Scripts/Environment/Obstacle.cs
@@ -6,6 +6,17 @@ public class Obstacle : MonoBehaviour
     private float damageInterval = 1f;
     private float damageTimer = 0f;
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
+
+            if (playerController != null)
+                playerController.TakeDamage(0.1f);
+        }
+    }
+
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
